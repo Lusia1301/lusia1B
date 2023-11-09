5c65e8ab5f1e1a3c8309a946de4a86093fd4b338
Lusia1301 <bareklusia1@gmail.com>
2023-11-10 00:23:06 +0700

    update

--- script.js
+++
@@ -1,6 +0,0 @@
-function img() {
-  document.getElementById("gantiGambar").src = "images/cantik.jpg";
-  document.getElementById("gantiLatar").src = "images/download.jpg";
-  document.getElementById("gantiFoto").src = "images/pulang.jpg";
-  document.getElementById("gantiNovel").src = "images/rinai.jpg";
-}
